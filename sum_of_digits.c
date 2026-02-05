99374ac95d23fa44f564bf3a57fdebc9b03622f8
aswnkv <aswnkvinod@gmail.com>
2026-02-05 15:57:07 +0000

    sum_of_digits.c

---
+++ sum_of_digits.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    scanf("%d", &num);
+
+    if (num < 0)
+        num = -num;
+
+    while (num > 0) {
+        sum += num % 10;  // get last digit
+        num /= 10;       // remove last digit
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
